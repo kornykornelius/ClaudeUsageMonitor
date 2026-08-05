bed108e8f4f572c65d647ceb7a8423eb95613127
Lew Woei Chwan <woeichwan1213@gmail.com>
2026-08-05 17:39:00 +0800

    Initial Commit

---
+++ ClaudeUsageMonitor/ClaudeUsageMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClaudeUsageMonitorApp.swift
+//  ClaudeUsageMonitor
+//
+//  Created by Lew Woei Chwan on 05/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct ClaudeUsageMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
